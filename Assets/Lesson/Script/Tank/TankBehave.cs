3db6ce32f7d533e164094342b1ab95c5483e654d
BillTruong <billtruong003@gmail.com>
2023-12-25 05:12:40 +0700

    *update

--- Assets/Lesson/Script/Tank/TankBehave.cs
+++ Assets/Lesson/Script/Tank/TankBehave.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class TankBehave : MonoBehaviour
 {
+    [SerializeField] private Animator anim;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotateSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
